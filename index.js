27c7959dce2b0e2549923df9e41c283ab33eb43f
mrdeangray <mrdeangray@gmail.com>
2022-03-21 19:46:50 -0400

    initial commit  - working products page

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express')
+const res = require('express/lib/response')
+const app = express()
+const path = require('path')
+const mongoose = require('mongoose')
+const methodOverride = require('method-override')
+app.use(methodOverride('_method'))
+app.use(express.static('public'));
+
+
+//const Product = require('./models/product')
+
+mongoose.connect('mongodb://127.0.0.1:27017/farm5', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log("MONGO CONNECTION OPEN!!!")
+    })
+    .catch(err => {
+        console.log("OH NO MONGO CONNECTION ERROR!!!!")
+        console.log(err)
+    })
+
+
+
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: true }));
+
+
+app.listen(3000, ()=>{
+    console.log("APP is listening on port 3000")
+})
+
+app.get("/products", (req, res)=>{
+    res.send(`Products`)
+
+})
