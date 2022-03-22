6d0889b192107db64325a2e9d6e4c14e36a1e50f
mrdeangray <mrdeangray@gmail.com>
2022-03-21 22:03:03 -0400

    added all CURD opps

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ app.use(methodOverride('_method'))
 app.use(express.static('public'));
 
 
-//const Product = require('./models/product')
+const Product = require('./models/product')
+const { redirect } = require('express/lib/response')
 
 mongoose.connect('mongodb://127.0.0.1:27017/farm5', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
@@ -31,7 +32,59 @@ app.listen(3000, ()=>{
     console.log("APP is listening on port 3000")
 })
 
-app.get("/products", (req, res)=>{
-    res.send(`Products`)
+//new
+app.get("/products/new", (req, res)=>{
+    //const product = new Product.find()
+    res.render("products/new")
+})
+
+//create post
+app.post("/products", async (req, res)=>{
+    const product = new Product(req.body)
+    await product.save()
+    res.redirect(`/products/${product._id}`)
+
+})
+
+//show
+app.get("/products/:id", async (req, res)=>{
+    const {id} = req.params
+    const product = await Product.findById(id)
+   //res.send(`show ${product}`)
+    res.render("products/show" , {product})
+})
+
+
+//update- get
+app.get("/products/:id/update", async (req, res)=>{
+    const {id} = req.params
+    const product = await Product.findById(id)
+   //res.send(`update ${product}`)
+    res.render("products/update", {product})
+})
+
+//update- put
+app.put("/products/:id", async (req, res)=>{
+    const {id} = req.params
+    const product = await Product.findByIdAndUpdate(id, req.body)
+    res.redirect(`/products/${product.id}`)
+    //res.send(`update and show ${product}`)
+
+    //res.render("products/new")
+})
+
+//delete
+app.delete("/products/:id", async(req, res)=>{
+    const {id} = req.params
+    const product = await Product.findByIdAndDelete(id)
+    res.redirect("/products")
+   
+
+})
+
+//products
+app.get("/products", async(req, res)=>{
+    const products = await Product.find({})
+    res.render("products/index", {products})
 
 })
